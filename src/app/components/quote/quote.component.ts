af7a80848fc6a8abc0db3e4038093604edafd159
changawa-antony <antony.changawa@student.moringaschool.com>
2021-05-17 23:30:31 +0300

    component updated

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -10,9 +10,9 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    {id:1, quote:'It is better to fail in originality than to succeed in imitation.',author:'Herman Melville',uploader: 'Kazungu'},
-    {id:2,quote:'Success usually comes to those who are too busy to be looking for it.',author:'Henry David Thoreau' ,uploader: 'Kazungu'},
-    {id:3,quote:'Try not to become a man of success. Rather become a man of value.',author:'Albert Einstein',uploader: 'Kazungu'},
+    {quote:'It is better to fail in originality than to succeed in imitation.',author:'Herman Melville',uploader: 'Kazungu'},
+    {quote:'Success usually comes to those who are too busy to be looking for it.',author:'Henry David Thoreau' ,uploader: 'Kazungu'},
+    {quote:'Try not to become a man of success. Rather become a man of value.',author:'Albert Einstein',uploader: 'Kazungu'},
     
   ];
   constructor() { }
